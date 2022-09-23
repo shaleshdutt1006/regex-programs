9b9e499c9accc7d3ec346e67084c7ce9783d0602
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-23 15:26:38 +0530

    Refactored : Validating the condition of Email Address using Regex

--- com/bridgelabz/Regex.java
+++ com/bridgelabz/Regex.java
@@ -5,48 +5,36 @@ import java.util.regex.*;
 
 public class Regex {
 
+    public static String validateEmail(String email) {
+
+        String emailRegex = ("[abc]{1,3}" + "[.]" + "[a-z 0-9]{1,}" + "[@]" + "bl{1}" + "[.]" + "co{1}" + "[.]" + "[a-z]{1,}");
+        Pattern pattern = Pattern.compile(emailRegex);
+        if (pattern.matcher(email).matches()) {
+            return "Valid Email";
+        } else {
+            return "Invalid Email";
+        }
+
+    }
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         /*
-        taking first name and last name as input by the user
+        taking email as input by the user
          */
-        System.out.println("Enter First Name");
-        String firstname = scanner.next();
-        System.out.println("Enter Last Name");
-        String lastname = scanner.next();
+        System.out.println("Enter Email to Check valid or invalid");
+        String email = scanner.next();
         scanner.close();
-        /*
-       Last Name has first letter capital and at least three character is in last name
-         */
-
-            /*
-            taking input from the user if both condition first and last name satisfies
-            then it shows true otherwise false
-             */
-        boolean result1 = firstname.matches("^[A-Z a-z]{2,}");
-        boolean result2 = lastname.matches("[A-Z]" + "[a-z]{2,}");
-
-        if (result1 == true && result2 == true) {
-            System.out.println("Validate condition");
-        } else {
-            System.out.println("Not validate");
-        }
-
-        /*
-        Second way to validate the name
-        condition \s denotes space between first name and last name
-        $ sign used to matches end of string
-         */
-        Pattern p = Pattern.compile("^[A-Z a-z]{2,}" + "\s" + "$[A-Z]" + "[a-z]{2,}");
-        Boolean result3 = p.compile("^[A-Z a-z]{2,}" + "\s" + "[A-Z]" + "[a-z]{2,}").matcher("Shalesh Dutt").matches();
-        System.out.println(result3);
-        /*
-        Third way to achieve the matching condition
-         */
-        boolean result4 = Pattern.matches("^[A-Z a-z]{2,}" + "\s" + "[A-Z]" + "[a-z]{2,}", "aman sharma");
-        System.out.println(result4);
+        System.out.println(validateEmail(email));
+  /*
+  Second way to get output
+  bl{1} means bl should be compulsory one time
+   */
+        Boolean result = Pattern.compile("^[abc]{3,}" + "[.]" + "[a-z 0-9]{1,}" + "[@]" + "bl{1}" + "[.]" + "co{1}" + "[.]" + "[a-z]{1,}").matcher("abc.xyz@al.co.in").matches();
+        System.out.println(result);
+        Boolean result1 = Pattern.compile("^[abc]{3,}" + "[.]" + "[a-z 0-9]{1,}" + "[@]" + "bl{1}" + "[.]" + "co{1}" + "[.]" + "[a-z]{1,}").matcher("abc.pq9@bl.co.in").matches();
+        System.out.println(result1);
 
 
     }
