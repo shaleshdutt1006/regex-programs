0a264ef2cfb8341318908e87d25ca3dc501f50bc
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-23 18:54:07 +0530

    Refactored : Condition of Password At least One Special Character

--- com/bridgelabz/Regex.java
+++ com/bridgelabz/Regex.java
@@ -5,10 +5,18 @@ import java.util.regex.*;
 
 public class Regex {
     /**
-     * Setting passowrd which has at least 8 character one uppercase letter and one numeric number
-     *
-     * @param args
+     * Setting password which has at least 8 character one uppercase letter and one numeric number and
+     * at least one special character and no whitespace allowed
      */
+    public static void checkPassword(String password) {
+
+        Pattern pattern = Pattern.compile("(?=.*[A-Z])" + "(?=.*[0-9])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$)" + ".{8,}");
+        Matcher matcher = pattern.matcher(password);
+        boolean result = matcher.matches();
+        System.out.println(result);
+
+    }
+
 
     public static void main(String[] args) {
 
@@ -17,17 +25,19 @@ public class Regex {
         taking Password as input by the user
          */
         System.out.println("Enter Password");
-        String password = scanner.next();
+        String password1 = scanner.next();
+        System.out.println("Enter Second Password");
+        String password2 = scanner.next();
         scanner.close();
+        checkPassword(password1);
     /*
-    Password has at least 8 character  one numeric number and one uppercase letter
-    . shows anything it can take as input or ? shows that it Matches up
-    exactly upon one character and * means matches zero or more time the preceding character
+     for at least one uppercase letter this equation uses (?=.*[A-Z]) , for at least one digit (?=.*[0-9])
+     for no whitespace allowed in the string this one uses (?=.*[@#$%^&+=])
      */
-        Pattern pattern = Pattern.compile("(?=.*[A-Z])" + "(?=.*[0-9])" + ".{8,}");
-        Matcher matcher = pattern.matcher(password);
+        Pattern pattern = Pattern.compile("(?=.*[A-Z])" + "(?=.*[0-9])" + "(?=.*[@#$%^&+=])" + ".{8,}");
 
-        if (pattern.matcher(password).matches()) {
+
+        if (pattern.matcher(password2).matches()) {
             System.out.println("Password is set");
 
         } else {
@@ -35,10 +45,10 @@ public class Regex {
         }
 
     /*
-    if we write .{8} in middle or starting then it will not work we have to first add conditions
-     we want uppercase lowercase then number of characters we want
+    if we write .{8} in middle or starting then it will not work we have to first add conditions we want uppercase
+     lowercase then number of characters. In this whitespace is allowed but in above two it is not allowed
      */
-        Boolean result = Pattern.compile("(?=.*[A-Z])" + "(?=.*[0-9])" + ".{8,}").matcher("Password123").matches();
+        Boolean result = Pattern.compile("(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "(?=.*[0-9])" + ".{8,}").matcher("Pass word123#").matches();
         System.out.println(result);
 
     }
