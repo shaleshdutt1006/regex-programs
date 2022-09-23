a3e2fe2dd67067e6bdd2bca3943207d913796937
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-23 13:04:26 +0530

    Refactored : Validating the condition of Last name with Regex

--- com/bridgelabz/Regex.java
+++ com/bridgelabz/Regex.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
 import java.util.regex.*;
 
 public class Regex {
@@ -7,26 +8,45 @@ public class Regex {
 
     public static void main(String[] args) {
 
-
-        Regex regex = new Regex();
+        Scanner scanner = new Scanner(System.in);
         /*
-        Name has first letter capital and at least three character is in first name
+        taking first name and last name as input by the user
          */
-        Pattern p = Pattern.compile("^[A-Z ]" + "[a-z]{2,}");
-
-        Matcher b = p.matcher("Dew");
-        boolean result = b.matches();
-        System.out.println(result);
+        System.out.println("Enter First Name");
+        String firstname = scanner.next();
+        System.out.println("Enter Last Name");
+        String lastname = scanner.next();
+        scanner.close();
         /*
-        Second way to validate the name condition
+       Last Name has first letter capital and at least three character is in last name
          */
-        Boolean result2 = p.compile("^[A-Z ]" + "[a-z]{2,}").matcher("Shalesh").matches();
-        System.out.println(result2);
+
+            /*
+            taking input from the user if both condition first and last name satisfies
+            then it shows true otherwise false
+             */
+        boolean result1 = firstname.matches("^[A-Z a-z]{2,}");
+        boolean result2 = lastname.matches("[A-Z]" + "[a-z]{2,}");
+
+        if (result1 == true && result2 == true) {
+            System.out.println("Validate condition");
+        } else {
+            System.out.println("Not validate");
+        }
+
+        /*
+        Second way to validate the name
+        condition \s denotes space between first name and last name
+        $ sign used to matches end of string
+         */
+        Pattern p = Pattern.compile("^[A-Z a-z]{2,}" + "\s" + "$[A-Z]" + "[a-z]{2,}");
+        Boolean result3 = p.compile("^[A-Z a-z]{2,}" + "\s" + "[A-Z]" + "[a-z]{2,}").matcher("Shalesh Dutt").matches();
+        System.out.println(result3);
         /*
         Third way to achieve the matching condition
          */
-        boolean result3 = Pattern.matches("^[A-Z ]" + "[a-z]{2,}", "aman");
-        System.out.println(result3);
+        boolean result4 = Pattern.matches("^[A-Z a-z]{2,}" + "\s" + "[A-Z]" + "[a-z]{2,}", "aman sharma");
+        System.out.println(result4);
 
 
     }
