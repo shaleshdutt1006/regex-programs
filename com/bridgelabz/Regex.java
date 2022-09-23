dc91bba6202809b2486762c5ea5628c8f299e288
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-23 17:45:54 +0530

    Refactored : Condition of Password of Minimum 8 Characters

--- com/bridgelabz/Regex.java
+++ com/bridgelabz/Regex.java
@@ -14,10 +14,10 @@ public class Regex {
         System.out.println("Enter Password");
         String password = scanner.next();
         scanner.close();
-/*
-Password has at least 8 character . shows anything that we can take anything uppercase lowercase letter
-or number or special character but at least 8 inputs should be there
- */
+    /*
+    Password has at least 8 character . shows anything that we can take anything uppercase lowercase letter
+    or number or special character but at least 8 inputs should be there
+     */
         Pattern pattern = Pattern.compile(".{8,}");
         Matcher matcher = pattern.matcher(password);
 
@@ -28,11 +28,11 @@ or number or special character but at least 8 inputs should be there
             System.out.println("Please Enter valid input Password is not Set");
         }
 
-/*
-Second Method to achieve output
- */
-Boolean result = Pattern.compile("[a-z A-Z 0-9]{8,}").matcher("Password123").matches();
-System.out.println(result);
+    /*
+    Second Method to achieve output
+     */
+        Boolean result = Pattern.compile("[a-z A-Z 0-9]{8,}").matcher("Password123").matches();
+        System.out.println(result);
 
     }
 }
